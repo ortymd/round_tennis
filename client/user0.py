d37c03ec93d2cf6a06e4b4c06cfbfe7e97e787d9
ortymd <polyarush.dmytro@gmail.com>
2018-05-27 12:15:48 +0300

    minor formatting updates

--- client/user0.py
+++ client/user0.py
@@ -1,9 +1,8 @@
 import ctypes
-import player
-import field
 import pygame
 from pygame.locals import *
 import threading
+import player
 
 # we use this handle to call functions from lib_test.so
 lib_handle=ctypes.cdll.LoadLibrary(
@@ -11,31 +10,43 @@ lib_handle=ctypes.cdll.LoadLibrary(
 
 def read_keyboard():
     pygame.event.clear()
-        while True:
-            print("start")
-            event = pygame.event.wait()    # here we wait until user hits keyboard
-            print("end")
-            player_input = ''
-            font = pygame.font.Font(None, 50)
-            if event.type == KEYDOWN:
-                if event.unicode == 'h':
-                    player_input = 'hello'
-                elif event.type == QUIT:
-                    return
+    while True:
+        print("start")
+        event = pygame.event.wait()    # here we wait until user hits keyboard
+        print("end")
+        player_input = ''
+        font = pygame.font.Font(None, 50)
+        if event.type == KEYDOWN:
+            if event.unicode == 'h':
+                player_input = 'hello'
+            elif event.type == QUIT:
+                return
 
-            screen.fill ((0, 0, 0))
-            block = font.render(player_input, True, (255, 255, 255))
-            rect = block.get_rect()
-            rect.center = screen.get_rect().center
-            screen.blit(block, rect)
-            pygame.display.flip()
+        screen.fill ((0, 0, 0))
+        block = font.render(player_input, True, (255, 255, 255))
+        rect = block.get_rect()
+        rect.center = screen.get_rect().center
+        screen.blit(block, rect)
+        pygame.display.flip()
 
-#create thread to get key input
+def update_screen():
+    return
+
+def read_game_updates():
+    return
+
+# create
+read_game_updates_thread = threading.Thread(target = read_game_updates)
+
+# create thread to update/display screen
+update_screen_thread = threading.Thread(target = update_screen)
+
+# create thread to get key input
 read_keyboard_thread = threading.Thread(target = read_keyboard)
 
-#create thread to display field
-#..TODO..
-
-pygame.init()
+pygame.init()    #  here we start all of the pygame stuff
 screen = pygame.display.set_mode((480, 360))
-read_keyboard_thread.start()
+read_game_updates_thread.start()
+update_screen_thread.start()
+read_keyboard_thread.start()  #  main thread does not wait for read_keyboard_thread.
+                              #  it exits once read_keyboard_thread started          
