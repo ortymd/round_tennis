546d91a6166fb9b43fea02445967cc7da1872c17
Dmytro Polyarush <polyarush.dmytro@gmail.com>
2018-05-29 15:47:09 +0300

    added test.c

---
+++ lib_c/test.c
@@ -0,0 +1,72 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lib_send_receive.h"
+
+typedef struct 
+{
+  char *data;
+  unsigned data_sz;
+  char mac_dest[6];
+} payload;
+
+void* test_send_data (void *test_payload)
+{
+  int res = 0;
+  payload *test_payload_ptr = (payload*)test_payload;
+
+  res = send_data (test_payload_ptr->data, test_payload_ptr->data_sz,
+               test_payload_ptr->mac_dest);
+
+  if (res == -1)
+    perror ("Error: send()\t");
+
+  return test_payload;
+}
+
+void* test_receive (void* buf)
+{
+  int res = 0;
+  
+  res = receive (buf);
+  if (res == -1)
+    perror ("Error: receive()\t");
+
+  return buf;
+}
+
+
+int main()
+{
+  int res = 0;
+
+  char buf[50];
+
+  const int sz = 50;
+  char mac_dest[6] = {0xff};
+  payload test_payload;
+
+  test_payload.data = "this is a test messsage";  /* setup test payload */
+  test_payload.data_sz = sz;
+  memcpy (test_payload.mac_dest, mac_dest, 6);
+
+  pthread_t send_data_thread;
+  pthread_t receive_thread;
+
+  socket_init();    /* initialize common socket for both threads */
+
+  res = pthread_create (&send_data_thread, 0, test_send_data, (void*)&test_payload);
+  if (res == -1)
+    perror("Error: pthread_create()\t");
+
+  res = pthread_create (&receive_thread, 0, test_receive, (void*)buf);
+  if (res == -1)
+    perror("Error: pthread_create()\t");
+
+  pthread_join (send_data_thread, NULL);
+  pthread_join (receive_thread, NULL);
+
+  exit(0);
+
+}
