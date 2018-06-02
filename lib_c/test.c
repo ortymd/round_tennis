84f386320cc2341d8ce1886c70ca566ff1ff5cd9
Dmytro Polyarush <polyarush.dmytro@plvision.eu>
2018-06-02 13:13:53 +0300

    Changed data to payload in identifiers.

--- lib_c/test.c
+++ lib_c/test.c
@@ -14,30 +14,30 @@
 
 typedef struct      /* need to put all here in one structure in order to pass to thread */
 {
-  char *data;
-  unsigned data_sz;
+  char *payload;
+  unsigned payload_sz;
   char mac_dest[6];
-} payload;
+} frame;
 
-void* test_send_data (void *test_payload)
+void* test_send_payload (void *test_frame)
 {
 #if INCLUDE_SEND == 1
   int res = 0;
   
-  payload *test_payload_ptr = (payload*)test_payload;
+  frame *test_frame_ptr = (frame*)test_frame;
 
-  printf("Starting send_data thread.\n"); 
+  printf("Starting send_payload thread.\n"); 
   for (unsigned i = 0; i < 10; ++i)
   {
-    res = send_data (test_payload_ptr->data, test_payload_ptr->data_sz,
-                    test_payload_ptr->mac_dest);
+    res = send_payload (test_frame_ptr->payload, test_frame_ptr->payload_sz,
+                    test_frame_ptr->mac_dest);
     if (res == -1)
       perror ("Error: send()\t");
     else
       printf ("***Message sent. ID:\t%u Sent bytes:\t%d\n", i, res);
   }
 #endif
-  return test_payload;
+  return test_frame;
 }
 
 void* test_receive (void* buf)
@@ -70,19 +70,19 @@ void* test_receive (void* buf)
 int main()
 {
   int res = 0;
-  char *data = "this is a test messsage";
-  unsigned data_sz = strlen (data);
+  char *payload = "this is a test messsage";
+  unsigned payload_sz = strlen (payload);
 
-  payload test_payload;                        /* set up payload */
-  test_payload.data = malloc (ETH_DATA_LEN);   /* get part of memory that we can modify (append 0 to payload) */
-  memcpy (test_payload.data, data, data_sz);
-  test_payload.data_sz = data_sz;
-  memset (test_payload.mac_dest, 0xff, ETH_ALEN);
+  frame test_frame;                        /* set up payload */
+  test_frame.payload = malloc (ETH_DATA_LEN);   /* get part of memory that we can modify (append 0 to payload) */
+  memcpy (test_frame.payload, payload, payload_sz);
+  test_frame.payload_sz = payload_sz;
+  memset (test_frame.mac_dest, 0xff, ETH_ALEN);
 
   char *buf = malloc (ETH_DATA_LEN);
   memset (buf, 0, ETH_DATA_LEN);
 
-  pthread_t send_data_thread;
+  pthread_t send_payload_thread;
   pthread_t receive_thread_1;
   pthread_t receive_thread_2;
 
@@ -90,7 +90,7 @@ int main()
   if (res == -1)
     perror("Error: socket_init()\t");
 
-  res = pthread_create (&send_data_thread, 0, test_send_data, (void*)&test_payload);
+  res = pthread_create (&send_payload_thread, 0, test_send_payload, (void*)&test_frame);
   if (res == -1)
     perror("Error: pthread_create()\t");
 
@@ -102,7 +102,7 @@ int main()
   if (res == -1)
     perror("Error: pthread_create()\t");
 
-  pthread_join (send_data_thread, NULL);
+  pthread_join (send_payload_thread, NULL);
   pthread_join (receive_thread_1, NULL);
   pthread_join (receive_thread_2, NULL);
 
